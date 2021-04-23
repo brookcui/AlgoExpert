96f2ba0d9dedb7b05b036da5d0bcd125d160063d
Rong Cui <brookcui97@gmail.com>
2021-04-23 00:21:51 -0400

    [Hard] Min Number Of Jumps (Solution 2)
    Link:
        https://www.algoexpert.io/questions/Min%20Number%20Of%20Jumps
    
    Note:
        1. Think about Greedy when a problem can be solved by Dynamic Programming easily.

--- Hard/MinNumberOfJumps.java
+++ Hard/MinNumberOfJumps.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+// Solution 1
 class Program {
 	// O(n^2) time | O(n) space
   public static int minNumberOfJumps(int[] array) {
@@ -21,5 +22,37 @@ class Program {
 			}
 		}
     return dp[dp.length-1];
+  }
+}
+
+// Solution 2
+class Program {
+	// O(n) time | O(1) space
+  public static int minNumberOfJumps(int[] array) {
+    // Write your code here.
+		if (array.length <= 1) {
+			return 0;
+		}
+		if (array[0] == 0) {
+			return -1;
+		}
+		int maxReach = array[0];
+		int step = array[0];
+		int jump = 1;
+		for (int i = 1; i < array.length; i++) {
+			if (i == array.length - 1) {
+				return jump;
+			}
+			maxReach = Math.max(maxReach, i + array[i]);
+			step--;
+			if (step == 0) {
+				jump++;
+				if (i >= maxReach) {
+					return -1;
+				}
+				step = maxReach - i;
+			}
+		}
+		return -1;
   }
 }
