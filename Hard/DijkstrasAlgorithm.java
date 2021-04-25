0d0df8860129cdb357034b204c06f9f266de4188
Rong Cui <brookcui97@gmail.com>
2021-04-25 18:21:01 -0400

    [Hard] Dijkstra's Algorithm (Solution 2)
    Link:
        https://www.algoexpert.io/questions/Dijkstra's%20Algorithm
    
    Note:
        1. Using heap can efficiently keep track of the shortest/longest path so far.

--- Hard/DijkstrasAlgorithm.java
+++ Hard/DijkstrasAlgorithm.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+// Solution 1
 class Program {
   public int[] dijkstrasAlgorithm(int start, int[][][] edges) {
     // Write your code here.
@@ -51,4 +52,41 @@ class Program {
 		}
 		return new int[] {vertex, minDist};
 	}
+}
+
+// Solution 2
+class Program {
+	// O((v + e) * log(v)) time | O(v) space - where v is the number of vertices and e is the number of edges
+  public int[] dijkstrasAlgorithm(int start, int[][][] edges) {
+    // Write your code here.
+		int numOfVertices = edges.length;
+		int[] minDistances = new int[numOfVertices];
+		Arrays.fill(minDistances, Integer.MAX_VALUE);
+		minDistances[start] = 0;
+		PriorityQueue<int[]> pq = new PriorityQueue<>((pair1, pair2) -> pair1[0] - pair2[0]);
+		pq.add(new int[]{start, 0});
+		while (!pq.isEmpty()) {
+			int[] pair = pq.poll();
+			int vertex = pair[0];
+			int minDist = pair[1];
+			if (minDist == Integer.MAX_VALUE) {
+				break;
+			}
+			for (int[] edge : edges[vertex]) {
+				int destination = edge[0];
+				int distToDestination = edge[1];
+				int newDist = minDist + distToDestination;
+				if (newDist < minDistances[destination]) {
+					minDistances[destination] = newDist;
+					pq.add(new int[]{destination, newDist});
+				}
+			}
+		}
+		for (int i = 0; i < numOfVertices; i++) {
+			if (minDistances[i] == Integer.MAX_VALUE) {
+				minDistances[i] = -1;
+			}
+		}
+    return minDistances;
+  }
 }
