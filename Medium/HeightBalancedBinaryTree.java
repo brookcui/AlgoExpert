a09a47ec6bb7b0e99d96efbbf2b2afa3e536dc4f
Rong Cui <brookcui97@gmail.com>
2021-04-26 18:49:08 -0400

    [Medium] Height Balanced Binary Tree (Solution 2)
    Link:
        https://www.algoexpert.io/questions/Height%20Balanced%20Binary%20Tree
    
    Note:
        1. Bottom-up Recursion provides enough information and avoids redundant computation.

--- Medium/HeightBalancedBinaryTree.java
+++ Medium/HeightBalancedBinaryTree.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+// Solution 1
 class Program {
   // This is an input class. Do not edit.
   static class BinaryTree {
@@ -31,4 +32,53 @@ class Program {
 		}
 		return 1 + Math.max(height(node.left), height(node.right));
 	}
+}
+
+// Solution 2
+class Program {
+  // This is an input class. Do not edit.
+  static class BinaryTree {
+    public int value;
+    public BinaryTree left = null;
+    public BinaryTree right = null;
+
+    public BinaryTree(int value) {
+      this.value = value;
+    }
+  }
+
+	class Pair<U, V> {
+		U first;
+		V second;
+		
+		public Pair(U first, V second) {
+			this.first = first;
+			this.second = second;
+		}
+	}
+
+	// O(n) time | O(n) space
+  public boolean heightBalancedBinaryTree(BinaryTree tree) {
+    // Write your code here.
+    return helper(tree).second;
+  }
+
+  private Pair<Integer, Boolean> helper(BinaryTree node) {
+    // Write your code here.
+		if (node == null) {
+			return new Pair<>(-1, true);
+		}
+		Pair<Integer, Boolean> left = helper(node.left);
+		if (!left.second) {
+			return new Pair<>(-1, false);
+		}
+		Pair<Integer, Boolean> right = helper(node.right);
+		if (!right.second) {
+			return new Pair<>(-1, false);
+		}
+		if (Math.abs(left.first - right.first) < 2) {
+			return new Pair<>(Math.max(left.first, right.first) + 1, true);
+		}
+    return new Pair<>(-1, false);
+  }
 }
