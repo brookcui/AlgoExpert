4c8689412827d1f8eceea9097a8f3905487f6ea2
Rong Cui <brookcui97@gmail.com>
2021-04-26 19:17:27 -0400

    [Medium] Reconstruct BST (Solution 2)
    Link:
        https://www.algoexpert.io/questions/Reconstruct%20BST
    
    Note:
        1. Binary Search Tree.

--- Medium/ReconstructBST.java
+++ Medium/ReconstructBST.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+// Solution 1
 class Program {
   // This is an input class. Do not edit.
   static class BST {
@@ -33,4 +34,44 @@ class Program {
 		bst.right = rightSubtree;
     return bst;
   }
+}
+
+// Solution 2
+class Program {
+  // This is an input class. Do not edit.
+  static class BST {
+    public int value;
+    public BST left = null;
+    public BST right = null;
+
+    public BST(int value) {
+      this.value = value;
+    }
+  }
+
+	// O(n) time | O(n) space
+  public BST reconstructBst(ArrayList<Integer> preOrderTraversalValues) {
+    // Write your code here.
+		if (preOrderTraversalValues.size() == 0) {
+			return null;
+		}
+    return helper(Integer.MIN_VALUE, Integer.MAX_VALUE, preOrderTraversalValues, new int[] {0});
+  }
+	
+	private BST helper(int lower, int upper, List<Integer> values, int[] rootIdx) {
+		if (rootIdx[0] == values.size()) {
+			return null;
+		}
+		int rootValue = values.get(rootIdx[0]);
+		if (rootValue < lower || rootValue >= upper) {
+			return null;
+		}
+		rootIdx[0] += 1;
+		BST leftSubtree = helper(lower, rootValue, values, rootIdx);
+		BST rightSubtree = helper(rootValue, upper, values, rootIdx);
+		BST bst = new BST(rootValue);
+		bst.left = leftSubtree;
+		bst.right = rightSubtree;
+		return bst;
+	}
 }
