c1365ae3df8e2a194f0e0a80dd3f064fd5721894
Rong Cui <brookcui97@gmail.com>
2021-04-26 15:23:02 -0400

    [Hard] Longest Substring Without Duplication (Solution 2)
    Link:
        https://www.algoexpert.io/questions/Longest%20Substring%20Without%20Duplication
    
    Note:
        1. Use hash table to keep track of the last position at which you see each element.

--- Hard/LongestSubstringWithoutDuplication.java
+++ Hard/LongestSubstringWithoutDuplication.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+// Solution 1
 class Program {
 	// O(n) time | O(min(m,n)) space - where n is the length of the string and m is the size of the charset
   public static String longestSubstringWithoutDuplication(String str) {
@@ -21,6 +22,32 @@ class Program {
 				longestRange[0] = l;
 				longestRange[1] = r + 1;
 			}
+		}
+    return str.substring(longestRange[0], longestRange[1]);
+  }
+}
+
+// Solution 2
+class Program {
+	// O(n) time | O(min(m, n)) space - where n is the length of the string and m is the size of the charset
+  public static String longestSubstringWithoutDuplication(String str) {
+    // Write your code here
+		if (str.length() == 0) {
+			return "";
+		}
+		int[] longestRange = {0, 1};
+		Map<Character, Integer> lastSeen = new HashMap<>();
+		int l = 0;
+		for (int r = 0; r < str.length(); r++) {
+			char ch = str.charAt(r);
+			if (lastSeen.containsKey(ch)) {
+				l = Math.max(l, lastSeen.get(ch) + 1);
+			}
+			if (r + 1 - l > longestRange[1] - longestRange[0]) {
+				longestRange[0] = l;
+				longestRange[1] = r + 1;
+			}
+			lastSeen.put(ch, r);
 		}
     return str.substring(longestRange[0], longestRange[1]);
   }
