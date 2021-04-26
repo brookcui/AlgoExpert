08914fc371334660065d4e2107f1de9e2b36543a
Rong Cui <brookcui97@gmail.com>
2021-04-26 18:25:20 -0400

    [Medium] Staircase Traversal (Solution 2)
    Link:
        https://www.algoexpert.io/questions/Staircase%20Traversal
    
    Note:
        1. Dynamic Programming: Dynamic number of subproblems.

--- Medium/StaircaseTraversal.java
+++ Medium/StaircaseTraversal.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+// Solution 1
 class Program {
 	// O(k^n) time | O(n) space
   public int staircaseTraversal(int height, int maxSteps) {
@@ -15,5 +16,27 @@ class Program {
 			numOfWays += staircaseTraversal(height - step, maxSteps);
 		}
 		return numOfWays;
+  }
+}
+
+// Solution 2
+class Program {
+	// O(n * k) time | O(n) space
+  public int staircaseTraversal(int height, int maxSteps) {
+    // Write your code here.
+		if (maxSteps == 0) {
+			return -1;
+		} else if (height <= 1) {
+			return 1;
+		}
+		int[] dp = new int[height + 1];
+		dp[0] = 1;
+		dp[1] = 1;
+		for (int h = 2; h <= height; h++) {
+			for (int step = 1; step <= Math.min(maxSteps, h); step++) {
+				dp[h] = dp[h] + dp[h-step];
+			}
+		}
+    return dp[height];
   }
 }
