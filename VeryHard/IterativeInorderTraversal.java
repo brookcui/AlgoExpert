31ca61939b7157d1cf9358bb2be8f74e2b506888
Rong Cui <brookcui97@gmail.com>
2021-04-27 02:56:15 -0400

    [Very Hard] Iterative In-order Traversal (Solution 2)
    Link:
        https://www.algoexpert.io/questions/Iterative%20In-order%20Traversal
    
    Note:
        1. Morris In-order Binary Tree Traversal.

--- VeryHard/IterativeInorderTraversal.java
+++ VeryHard/IterativeInorderTraversal.java
@@ -1,6 +1,7 @@
 import java.util.function.Function;
 import java.util.*;
 
+// Solution 1
 class Program {
  	// O(n) time | O(n) space
   public static void iterativeInOrderTraversal(
@@ -19,6 +20,51 @@ class Program {
 			curr = stack.pop();
 			callback.apply(curr);
 			curr = curr.right;
+		}
+  }
+
+  static class BinaryTree {
+    public int value;
+    public BinaryTree left;
+    public BinaryTree right;
+    public BinaryTree parent;
+
+    public BinaryTree(int value) {
+      this.value = value;
+    }
+
+    public BinaryTree(int value, BinaryTree parent) {
+      this.value = value;
+      this.parent = parent;
+    }
+  }
+}
+
+// Solution 2
+class Program {
+	// O(n) time | O(1) space
+  public static void iterativeInOrderTraversal(
+      BinaryTree tree, Function<BinaryTree, Void> callback) {
+    // Write your code here.
+		BinaryTree prev = null;
+		BinaryTree curr = tree;
+		while (curr != null) {
+			BinaryTree next = null;
+			if (prev == null || prev == curr.parent) {
+				if (curr.left != null) {
+					next = curr.left;
+				} else {
+					callback.apply(curr);
+					next = (curr.right != null) ? curr.right : curr.parent;
+				}
+			} else if (prev == curr.left) {
+				callback.apply(curr);
+				next = (curr.right != null) ? curr.right : curr.parent;
+			} else {
+				next = curr.parent;
+			}
+			prev = curr;
+			curr = next;
 		}
   }
 
