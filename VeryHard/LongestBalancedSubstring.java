66ab9ae87ee988d2589cf524bbe58dde17afe29f
Rong Cui <brookcui97@gmail.com>
2021-04-27 15:33:04 -0400

    [Very Hard] Longest Balanced Substring (Solution 2)
    Link:
        https://www.algoexpert.io/questions/Longest%20Balanced%20Substring
    
    Note:
        1. Use two variables to keep track of the number of to be matched elements, as you traverse the input.

--- VeryHard/LongestBalancedSubstring.java
+++ VeryHard/LongestBalancedSubstring.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+// Solution 1
 class Program {
 	// O(n) time | O(n) space
   public int longestBalancedSubstring(String string) {
@@ -18,6 +19,48 @@ class Program {
 					int leftIdx = stack.peek();
 					maxLength = Math.max(maxLength, i - leftIdx);
 				}
+			}
+		}
+    return maxLength;
+  }
+}
+
+// Solution 2
+class Program {
+	// O(n) time | O(1) space
+  public int longestBalancedSubstring(String string) {
+    // Write your code here.
+		int maxLength = 0;
+		int opening = 0;
+		int closing = 0;
+		for (int i = 0; i < string.length(); i++) {
+			char c = string.charAt(i);
+			if (c == '(') {
+				opening++;
+			} else {
+				closing++;
+			}
+			if (opening == closing) {
+				maxLength = Math.max(maxLength, opening + closing);
+			} else if (closing > opening) {
+				opening = 0;
+				closing = 0;
+			}
+		}
+		opening = 0;
+		closing = 0;
+		for (int i = string.length() - 1; i >= 0; i--) {
+			char c = string.charAt(i);
+			if (c == ')') {
+				opening++;
+			} else {
+				closing++;
+			}
+			if (opening == closing) {
+				maxLength = Math.max(maxLength, opening + closing);
+			} else if (closing > opening) {
+				opening = 0;
+				closing = 0;
 			}
 		}
     return maxLength;
