8bc831463fb142cd200b9421ffa5cb611a468988
Rong Cui <brookcui97@gmail.com>
2021-04-27 02:44:16 -0400

    [Very Hard] Flatten Binary Tree (Solution 2)
    Link:
        https://www.algoexpert.io/questions/Flatten%20Binary%20Tree
    
    Note:
        1. At any given node in the in-order traversal order, the node immediately to its left is the rightmost node of its left subtree, and the node immediately its right is the leftmost node of its right subtree.

--- VeryHard/FlattenBinaryTree.java
+++ VeryHard/FlattenBinaryTree.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+// Solution 1
 class Program {
 	// O(n) time | O(n) space
   public static BinaryTree flattenBinaryTree(BinaryTree root) {
@@ -24,6 +25,55 @@ class Program {
 			getInorderNodes(root.right, inorderNodes);
 		}
 		return inorderNodes;
+	}
+
+  // This is the class of the input root. Do not edit it.
+  static class BinaryTree {
+    int value;
+    BinaryTree left = null;
+    BinaryTree right = null;
+
+    public BinaryTree(int value) {
+      this.value = value;
+    }
+  }
+}
+
+// Solution 2
+class Program {
+	// O(n) time | O(d) space
+  public static BinaryTree flattenBinaryTree(BinaryTree root) {
+    // Write your code here.
+		if (root == null) {
+			return null;
+		}
+		BinaryTree leftMost = flatten(root)[0];
+		return leftMost;
+  }
+	
+	private static BinaryTree[] flatten(BinaryTree node) {
+		BinaryTree leftMost;
+		BinaryTree rightMost;
+		if (node.left == null) {
+			leftMost = node;
+		} else {
+			BinaryTree[] leftAndRightMost = flatten(node.left);
+			connectNodes(leftAndRightMost[1], node);
+			leftMost = leftAndRightMost[0];
+		}
+		if (node.right == null) {
+			rightMost = node;
+		} else {
+			BinaryTree[] leftAndRightMost = flatten(node.right);
+			connectNodes(node, leftAndRightMost[0]);
+			rightMost = leftAndRightMost[1];
+		}
+		return new BinaryTree[] {leftMost, rightMost};
+	}
+
+	private static void connectNodes(BinaryTree leftNode, BinaryTree rightNode) {
+		leftNode.right = rightNode;
+		rightNode.left = leftNode;
 	}
 
   // This is the class of the input root. Do not edit it.
