a5b98903e4b4c1f041918e3810ecd824be2e7dd8
Rong Cui <brookcui97@gmail.com>
2021-04-27 02:17:44 -0400

    [Very Hard] All Kinds of Node Depths (Solution 2)
    Link:
        https://www.algoexpert.io/questions/All%20Kinds%20Of%20Node%20Depths
    
    Note:
        1. Fine-tune the return value of DFS function.

--- VeryHard/AllKindsOfNodeDepths.java
+++ VeryHard/AllKindsOfNodeDepths.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+// Solution 1
 class Program {
 	// O(nlog(n)) time | O(h) space
   public static int allKindsOfNodeDepths(BinaryTree root) {
@@ -17,6 +18,38 @@ class Program {
 		int left = nodeDepth(node.left, depth + 1);
 		int right = nodeDepth(node.right, depth + 1);
 		return depth + left + right;
+	}
+
+  static class BinaryTree {
+    int value;
+    BinaryTree left;
+    BinaryTree right;
+
+    public BinaryTree(int value) {
+      this.value = value;
+      left = null;
+      right = null;
+    }
+  }
+}
+
+// Solution 2
+class Program {
+	// O(n) time | O(h) space
+  public static int allKindsOfNodeDepths(BinaryTree root) {
+    // Write your code here.
+		if (root == null) {
+			return 0;
+		}
+		return dfs(root, 0, 0);
+  }
+
+	private static int dfs(BinaryTree node, int depth, int depthSum) {
+		if (node == null) {
+			return 0;
+		}
+		depthSum += depth;
+		return depthSum + dfs(node.left, depth + 1, depthSum) + dfs(node.right, depth + 1, depthSum);
 	}
 
   static class BinaryTree {
