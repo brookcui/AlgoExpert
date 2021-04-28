268b42c8014a3eff93bb037e99e92595140a047c
Rong Cui <brookcui97@gmail.com>
2021-04-28 14:37:36 -0400

    [Very Hard] Number Of Binary Tree Topologies (Solution 2)
    Link:
        https://www.algoexpert.io/questions/Number%20Of%20Binary%20Tree%20Topologies
    
    Note:
        1. DFS + Memoization.

--- VeryHard/NumberOfBinaryTreeTopologies.java
+++ VeryHard/NumberOfBinaryTreeTopologies.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+// Solution 1
 class Program {
 	// O((n*(2n)!)/(n!(n+1)!)) | O(n) space
   public static int numberOfBinaryTreeTopologies(int n) {
@@ -16,4 +17,30 @@ class Program {
 		}
     return num;
   }
+}
+
+// Solution 2
+class Program {
+	// O(n^2) time | O(n) space
+  public static int numberOfBinaryTreeTopologies(int n) {
+    // Write your code here.
+		Map<Integer, Integer> memo = new HashMap<>();
+		memo.put(0, 1);
+		return dfs(n, memo);
+  }
+	
+	private static int dfs(int n, Map<Integer, Integer> memo) {
+		if (memo.containsKey(n)) {
+			return memo.get(n);
+		}
+		int num = 0;
+		for (int leftTreeSize = 0; leftTreeSize < n; leftTreeSize++) {
+			int rightTreeSize = n - 1 - leftTreeSize;
+			int numLeft = numberOfBinaryTreeTopologies(leftTreeSize);
+			int numRight = numberOfBinaryTreeTopologies(rightTreeSize);
+			num += numLeft * numRight;
+		}
+		memo.put(n, num);
+    return num;
+	}
 }
